4c29e04971d2688845315a894543ba7a9e4a300c
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2023-12-22 18:27:21 +0800

    Update Data_Augmentation.py

--- Data_Augmentation.py
+++ Data_Augmentation.py
@@ -1,5 +1,3 @@
-
-
 import PIL
 import numpy as np
 import torch
@@ -8,18 +6,21 @@ import torchvision
 
 ################# Remaining data augmentation for training (Top) and testing (Bottom) #################
 
-# The data augmentation selected for the train datasets are as below.
-
 data_transform = transforms.Compose(
-
      [
-            transforms.ToTensor(),
-            transforms.Resize((84,84)),
-            transforms.CenterCrop((84,84)),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.ToTensor(),
+        transforms.Resize(84),
+        transforms.CenterCrop(84),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
        ])
 
-############################ No augmentation for test dataset ############################
-data_transform_test = transforms.Compose([transforms.ToTensor(),
-                                          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+data_transform_test = transforms.Compose(
 
+      [
+        transforms.ToTensor(),
+        transforms.Resize(84),
+        transforms.CenterCrop(84),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+       ])
