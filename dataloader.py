e92063f97944b9673d816b04d8753ce225b70565
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2023-12-22 18:29:51 +0800

    Create dataloader.py

---
+++ dataloader.py
@@ -0,0 +1,25 @@
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+########################### miniImageNet dataloader ###################################
+
+class miniImageNet_CustomDataset(Dataset):
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = data_transform
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        image = self.images[idx]
+        image = self.transform(np.array(image))
+        return image, label
+    def __len__(self):
+        return len(self.labels)
+
+# new_X_train,new_y_train, new_X_test, new_y_test from 'Data_Preparation.py'
+
+train_dataset = miniImageNet_CustomDataset(new_X_train,new_y_train, transform=data_transform)
+test_dataset =  miniImageNet_CustomDataset(new_X_test, new_y_test, transform=data_transform_test)
+
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)  
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=True)   # 
