7b8b8a9ee6646555fafe078483df9da92053850e
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2023-12-24 14:03:16 +0800

    Create evaluate_on_one_task.py

---
+++ evaluate_on_one_task.py
@@ -0,0 +1,47 @@
+def evaluate_on_one_task(
+    support_images: torch.Tensor,
+    support_labels: torch.Tensor,
+    query_images: torch.Tensor,
+    query_labels:torch.Tensor,
+) -> [int, int]:
+    """
+    Returns the number of correct predictions of query labels, and the total number of predictions.
+    """
+    return (
+        torch.max(
+            model(support_images.cuda(), support_labels.cuda(), query_images.cuda())
+            .detach()
+            .data,
+            1,
+        )[1]
+        == query_labels.cuda()
+    ).sum().item(), len(query_labels)
+
+
+def evaluate(data_loader: DataLoader):
+    # We'll count everything and compute the ratio at the end
+    total_predictions = 0
+    correct_predictions = 0
+
+    # eval mode affects the behaviour of some layers (such as batch normalization or dropout)
+    # no_grad() tells torch not to keep in memory the whole computational graph (it's more lightweight this way)
+    model.eval()
+    with torch.no_grad():
+        for episode_index, (
+            support_images,
+            support_labels,
+            query_images,
+            query_labels,
+            class_ids,
+        ) in tqdm(enumerate(data_loader), total=len(data_loader)):
+
+            correct, total = evaluate_on_one_task(
+                support_images, support_labels, query_images, query_labels
+            )
+
+            total_predictions += total
+            correct_predictions += correct
+
+    print(
+        f"Model tested on {len(data_loader)} tasks. Accuracy: {(100 * correct_predictions/total_predictions):.2f}%"
+    )
