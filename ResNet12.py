b46c3acdfbcd2cfb6063527e238233d0ef95e58d
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2024-04-11 05:51:26 +0800

    Update ResNet12.py

--- ResNet12.py
+++ ResNet12.py
@@ -3,90 +3,140 @@ import torch
 from torch import Tensor, nn
 #from torchvision.models.resnet import BasicBlock, Bottleneck, conv1x1
 
-class ResNet(nn.Module):
-    def __init__(
-        self,
-        block: Type[Union[BasicBlock, Bottleneck]],
-        layers: List[int],
-        planes: Optional[List[int]] = None,
-        use_fc: bool = False,
-        num_classes: int = 100, #1000
-        use_pooling: bool = True,
-        big_kernel: bool = False,
-        zero_init_residual: bool = False,
-    ):
-        """
-        Custom ResNet architecture, with some design differences compared to the built-in
-        PyTorch ResNet.
-        This implementation and its usage in predesigned_modules is derived from
-        https://github.com/fiveai/on-episodes-fsl/blob/master/src/models/ResNet.py
-        Args:
-            block: which core block to use (BasicBlock, Bottleneck, or any child of one of these)
-            layers: number of blocks in each of the 4 layers
-            planes: number of planes in each of the 4 layers
-            use_fc: whether to use one last linear layer on features
-            num_classes: output dimension of the last linear layer (only used if use_fc is True)
-            use_pooling: whether to average pool the features (must be True if use_fc is True)
-            big_kernel: whether to use the shape of the built-in PyTorch ResNet designed for
-                ImageNet. If False, make the first convolutional layer less destructive.
-            zero_init_residual: zero-initialize the last BN in each residual branch, so that the
-                residual branch starts with zeros, and each residual block behaves like an identity.
-                This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        """
-        super().__init__()
-        if planes is None:
-            planes = [64, 128, 256, 512]
+import torch.nn as nn
+import torch
+from itertools import chain, combinations
 
-        self.inplanes = 64
+__all__ = ['resnet10', 'resnet12', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+           'resnet152']
 
-        # Built-in PyTorch ResNet uses a first conv layer with a 7*7 kernel and a stride of 2,
-        # which is fine for ImageNet's 224x224 images, but too destructive for 84x84 images
-        # which are commonly used in few-shot settings.
-        self.conv1 = (
-            nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=1, bias=False)
-            if big_kernel
-            else nn.Conv2d(
-                3, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False
-            )
-        )
-        self.bn1 = nn.BatchNorm2d(self.inplanes)
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.downsample = downsample
+        self.stride = stride
+
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = conv1x1(inplanes, planes)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes, stride)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = conv1x1(planes, planes * self.expansion)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.relu = nn.ReLU(inplace=True)
 
-        self.layer1 = self._make_layer(block, planes[0], layers[0])
-        self.layer2 = self._make_layer(block, planes[1], layers[1], stride=2)
-        self.layer3 = self._make_layer(block, planes[2], layers[2], stride=2)
-        self.layer4 = self._make_layer(block, planes[3], layers[3], stride=2)
+        self.downsample = downsample
+        self.stride = stride
 
-        self.use_pooling = use_pooling
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.sigmoid = nn.Sigmoid()
 
-        # Only used when self.use_fc is True
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):                         # 32, 64, 128, 256        # 512
+
+    def __init__(self, block, layers, widths=[32, 64, 128, 256], feature_dim=512, num_classes=1000, projection=False, zero_init_residual=False, drop_rate=0, use_fc=False):
+        super(ResNet, self).__init__()
+        if drop_rate > 0:
+            raise NotImplementedError()
         self.use_fc = use_fc
-        self.fc = nn.Linear(self.inplanes, num_classes)
+        self.projection = projection
+        self.inplanes = 64       #64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, widths[0], layers[0])
+        self.layer2 = self._make_layer(block, widths[1], layers[1], stride=2)
+        self.layer3 = self._make_layer(block, widths[2], layers[2], stride=2)
+        self.layer4 = self._make_layer(block, widths[3], layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        if self.projection:
+            self.proj1 = nn.Linear(widths[3]*block.expansion, feature_dim)
 
-        for module in self.modules():
-            if isinstance(module, nn.Conv2d):
-                nn.init.kaiming_normal_(
-                    module.weight, mode="fan_out", nonlinearity="relu"
-                )
-            elif isinstance(module, nn.BatchNorm2d):
-                nn.init.constant_(module.weight, 1)
-                nn.init.constant_(module.bias, 0)
+        if self.use_fc:
+            self.fc = nn.Linear(widths[3] * block.expansion, num_classes)
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
         if zero_init_residual:
-            for module in self.modules():
-                if isinstance(module, Bottleneck):
-                    nn.init.constant_(module.bn3.weight, 0)
-                elif isinstance(module, BasicBlock):
-                    nn.init.constant_(module.bn2.weight, 0)
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
 
-    def _make_layer(
-        self,
-        block: Type[Union[BasicBlock, Bottleneck]],
-        planes: int,
-        blocks: int,
-        stride: int = 1,
-    ) -> nn.Module:
+    def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
@@ -102,58 +152,46 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Forward pass through the ResNet.
-        Args:
-            x: input tensor of shape (batch_size, **image_shape)
-        Returns:
-            x: output tensor of shape (batch_size, num_classes) if self.use_fc is True,
-                otherwise of shape (batch_size, **feature_shape)
-        """
-        #x = self.layer4(
-            #self.layer3(self.layer2(self.layer1(self.relu(self.bn1(self.conv1(x))))))
-       #)
-
+    def forward(self, x, use_fc=False, cat=False):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
 
+        if cat and not self.training:
+            # returns intermediate layers for multi-eval evaluation
+            x2 = self.layer1(x)
+            x2_avg = self.avgpool(x2).view(x.size(0), -1)
+            x3 = self.layer2(x2)
+            x3_avg = self.avgpool(x3).view(x.size(0), -1)
+            x4 = self.layer3(x3)
+            x4_avg = self.avgpool(x4).view(x.size(0), -1)
+            x5 = self.layer4(x4)
 
-        if self.use_pooling:
-            x = torch.flatten(
-                self.avgpool(x),
-                1,
-            )
+            x5_avg = self.avgpool(x5).view(x.size(0), -1)
 
-            if self.use_fc:
-                return self.fc(x)
+            x = torch.cat([x3_avg, x4_avg, x5_avg], axis=1)
 
+            x1 = None
         else:
-            if self.use_fc:
-                raise ValueError(
-                    "You can't use the fully connected layer without pooling features."
-                )
+            x = self.layer1(x)
+            x = self.layer2(x)
+            x = self.layer3(x)
+            x = self.layer4(x)
+
+            x = self.avgpool(x)
+
+            x = x.view(x.size(0), -1)
+
+            x1 = self.fc(x) if use_fc else None
+
+        if self.training and self.projection:
+            x = self.proj1(x)
 
         return x
 
-    def set_use_fc(self, use_fc: bool):
-        """
-        Change the use_fc property. Allow to decide when and where the model should use its last
-        fully connected layer.
-        Args:
-            use_fc: whether to set self.use_fc to True or False
-        """
-        self.use_fc = use_fc
-
-###############################################################################################
-from torch import nn
-#from torchvision.models.resnet import BasicBlock, Bottleneck
-
-def resnet12(**kwargs) -> ResNet:
-    """Constructs a ResNet-12 model."""
-    return ResNet(BasicBlock, [1, 1, 2, 1], planes=[64, 160, 320, 640], **kwargs)
+def resnet12(**kwargs):
+    """Constructs a ResNet-12 model.
+    """
+   # print('\n>> Using custom ResNet12 architecture')  # 1, 1, 2, 1
+    model = ResNet(BasicBlock, [1, 1, 2, 1], widths=[32, 64, 128, 256], **kwargs)
+    return model
