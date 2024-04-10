45713b5013170032e66e3729acec8c3c0090a9a8
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2024-04-11 02:21:32 +0800

    Update Data_Augmentation.py

--- Data_Augmentation.py
+++ Data_Augmentation.py
@@ -1,15 +1,22 @@
-import PIL
-import numpy as np
-import torch
-import torchvision
 
+from torchvision.transforms import (
+    Compose,
+    RandomApply,
+    RandomHorizontalFlip,
+    RandomRotation,
+    RandomVerticalFlip,
+)
 
-################# Remaining data augmentation for training (Top) and testing (Bottom) #################
+from torchvision import transforms
+from torchvision.transforms import v2
+
+# The data augmentation selected for the train datasets are as below.
 
 data_transform = transforms.Compose(
 
      [
             transforms.ToTensor(),
+            #v2.RandAugment(),  # RandAugment
             transforms.Resize((84,84)),
             transforms.CenterCrop((84,84)),
             transforms.RandomVerticalFlip(0.5),
@@ -19,12 +26,43 @@ data_transform = transforms.Compose(
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
        ])
 
+
 data_transform_test = transforms.Compose(
 
       [
         transforms.ToTensor(),
-        transforms.Resize(84),
-        transforms.CenterCrop(84),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
        ])
+
+import torchvision.transforms as T
+
+# Based on the Augmentation procedure laid out in the simCLR paper, as our Hesim Loss has an analogous structure #
+
+class Augment:
+   """
+   A stochastic data augmentation module
+   Transforms any given data example randomly
+   resulting in two correlated views of the same example,
+   denoted x ̃i and x ̃j, which we consider as a positive pair.
+   """
+
+   def __init__(self, img_size, s=1):
+       color_jitter = T.ColorJitter(
+           0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s
+       )
+       # 10% of the image
+       blur = T.GaussianBlur((3, 3), (0.1, 2.0))
+
+       self.train_transform = torch.nn.Sequential(
+           T.RandomResizedCrop(size=img_size),
+           T.RandomHorizontalFlip(p=0.5), 
+           T.RandomApply([color_jitter], p=0.5),
+           T.RandomApply([blur], p=0.5),
+           T.RandomGrayscale(p=0.5), 
+           # imagenet stats
+           T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+       )
+
+   def __call__(self, x):
+       return self.train_transform(x), self.train_transform(x)
