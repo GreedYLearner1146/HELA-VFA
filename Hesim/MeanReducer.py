1fb93577d27f171ab25388f7d3a819aa040df266
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2024-04-12 20:45:30 +0800

    Create MeanReducer.py

---
+++ Hesim/MeanReducer.py
@@ -0,0 +1,14 @@
+import torch
+
+class MeanReducer(BaseReducer):
+    def element_reduction(self, losses, *_):
+        return torch.mean(losses)
+
+    def pos_pair_reduction(self, losses, *args):
+        return self.element_reduction(losses, *args)
+
+    def neg_pair_reduction(self, losses, *args):
+        return self.element_reduction(losses, *args)
+
+    def triplet_reduction(self, losses, *args):
+        return self.element_reduction(losses, *args)
