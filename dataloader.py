61f9a60ae9754206037822ef3344b97eded3e382
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2024-04-16 12:42:16 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -34,4 +34,4 @@ train_dataset = miniImageNet_CustomDataset(new_X_train,new_y_train, transform=[d
 val_dataset =  miniImageNet_CustomDataset(new_X_val,new_y_val, transform=[data_transform_valtest])
 
 train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, collate_fn=collate_fn) # Collate_fn called on here.
-val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=True) # Collate_fn called on here.
+val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=True) 
